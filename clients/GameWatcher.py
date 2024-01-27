cad4dc1936e7974ec1d401319f428aa8b516dbed
Fay Schut <theOnly1Rogue@gmail.com>
2024-01-27 11:58:00 +0100

    added move tracking to GameWatcher

--- clients/GameWatcher.py
+++ clients/GameWatcher.py
@@ -1,24 +1,29 @@
 import random
+from urllib.parse import quote_plus
+
 import websockets
 import asyncio
+
+from tak.board import TakBoard
 
 URI = "ws://www.playtak.com:9999/ws"
 
 
 CHARS = [chr(i+97) for i in range(26)] #all characters allowed for random tokens (a-z)
 
-
-
-
 #this class creates a guest login and keeps track of a single game
 class GameWatcher:
 
     tokens = []
 
-    def __init__(self, gameId):
+    def __init__(self, gameId, data):
         self.gameId = gameId
-
+        self.data = data
         self.moves = []
+
+        self.player = "white"
+        self.engine = TakBoard(data["size"], data["half_komi"])
+
 
         #create and log a unique token
         self.token = random.choices(CHARS, k=20)
@@ -44,16 +49,45 @@ class GameWatcher:
             msg = msg.split()[1:]
             match msg:
                 case ["M", sq1, sq2, *drops]:
-                    self.moves.append(msg) #todo, better repr
+                    self.makeMove(msg)
 
                 case ["P", sq, *piece]:
-                    self.moves.append(msg)  # todo, better repr
+                    self.makeMove(msg)
 
                 case ["Undo"]:
-                    self.moves = self.moves[:-1]
+                    self.undoMove()
 
                 case ["Abandoned.", player, "quit"]:
                     pass #todo parse game end
 
                 case ["Over", result]:
                     pass #todo parse game end
+
+    def makeMove(self, server_move):
+        move = self.engine.server_to_move(server_move)
+        self.moves.append(move)
+        self.engine.make_move(move, self.player)
+        self.player = self.engine.invert_player(self.player)
+
+
+    def undoMove(self):
+        move = self.moves[-1]
+        self.moves = self.moves[:-1]
+        self.player = self.engine.invert_player(self.player)
+        self.engine.undo_move(move, self.player)
+
+
+    def generate_image_link(self):
+        size, half_komi = self.data["size"], self.data["half_komi"]
+        caps, flats = self.data["capstones"], self.data["pieces"]
+
+        # have to ensure compat with URL
+        player_1, player_2 = quote_plus(self.data["player_1"]), quote_plus(self.data["player_2"])
+        theme = quote_plus(self.THEME)
+
+        tps = quote_plus(self.engine.position_to_TPS())  # quote_plus to ensure URL compat
+
+        last_move = "&hl=" + quote_plus(self.engine.move_to_ptn(self.moves[-1]))
+
+        return f"https://tps.ptn.ninja/?tps={tps}&imageSize=sm&caps={caps}&flats={flats}&player1={player_1}&player2={player_2}&name=game.png&theme={theme}" + last_move
+
